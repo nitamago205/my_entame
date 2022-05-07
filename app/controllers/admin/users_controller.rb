31fccdb258347edfd0641bea9dec267f10b5017f
nitamago205 <3ejiz428@gmail.com>
2022-05-07 11:34:16 +0000

    admin側、ユーザー機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,17 +1,38 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all.page(params[:page]).per(10)
   end
-  
+
   def show
+    @user = User.find(params[:id])
   end
-  
+
   def edit
+     @user = User.find(params[:id])
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render "edit"
+    end
   end
-  
+
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to admin_user_path(@user), notice: "ユーザーを削除しました。"
+    else
+      render "edit"
+    end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :is_deleted)
+  end
+
 end
