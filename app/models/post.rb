3bbe1a99a87bf4793c641ac1894104b98b27555b
nitamago205 <3ejiz428@gmail.com>
2022-05-12 05:19:19 +0000

    いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,14 @@ class Post < ApplicationRecord
   belongs_to :genre
   has_many :post_comments, dependent: :destroy
   has_many :my_selects, dependent: :destroy
-  
+  has_many :favorites, dependent: :destroy
+
+
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(rate: :desc)}
+
+  def favorited_by?(user)
+   favorites.where(user_id: user.id).exists?
+  end
 end
