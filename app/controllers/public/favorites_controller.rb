bfef981e4255227409ad8d529c1885fc5319dd03
nitamago205 <3ejiz428@gmail.com>
2022-05-27 14:39:20 +0000

    user詳細画面修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,20 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.new(post_id: @post.id)
     @favorite.save
-    # render 'replace_btn'
+    @q = Post.ransack(params[:q])
+
+
+
+    @user = User.find(params[:user_id])
+    @favorite_posts = @q.result(distinct: true).joins(:favorites).where(favorites: {user_id: @user.id}).page(params[:favorite_posts_page]).per(5).order(created_at: "DESC")
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.find_by(post_id: @post.id)
     @favorite.destroy
-    # render 'replace_btn'
+    @q = Post.ransack(params[:q])
+    @user = User.find(params[:user_id])
+    @favorite_posts = @q.result(distinct: true).joins(:favorites).where(favorites: {user_id: @user.id}).page(params[:favorite_posts_page]).per(5).order(created_at: "DESC")
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -10,9 +10,14 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @q = @user.posts.ransack(params[:q])
-    @posts = @q.result(distinct: true).page(params[:page]).per(10).order(created_at: "DESC")
     @genres = Genre.all
+
+    @q = Post.ransack(params[:q])
+    # @q = @user.posts.ransack(params[:q])
+    @posts = @q.result(distinct: true).where(user_id: @user).page(params[:posts_page]).per(5).order(created_at: "DESC")
+    @favorite_posts = @q.result(distinct: true).joins(:favorites).where(favorites: {user_id: @user.id}).page(params[:favorite_posts_page]).per(5).order(created_at: "DESC")
+    # Post.joins(:favorites).where(favorites: {user_id: @user.id})
+    @comment_posts = @q.result(distinct: true).joins(:post_comments).where(post_comments: {user_id: @user.id}).page(params[:comment_posts_page]).per(5).order(created_at: "DESC")
   end
 
   def edit
