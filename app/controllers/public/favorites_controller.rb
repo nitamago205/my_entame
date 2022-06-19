a4c9b221e7393e8c8179ed8d32e2e76772797cb6
nitamago205 <3ejiz428@gmail.com>
2022-06-19 02:56:09 +0000

    不要なコメントアウト削除

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,18 +5,11 @@ class Public::FavoritesController < ApplicationController
     @favorite.save
     #通知機能
     @post.create_notification_favorite!(current_user)
-
-    # @q = Post.ransack(params[:q])
-    # @user = User.find(params[:user_id])
-    # @favorite_posts = @q.result(distinct: true).joins(:favorites).where(favorites: {user_id: @user.id}).page(params[:favorite_posts_page]).per(6).order(created_at: "DESC")
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.find_by(post_id: @post.id)
     @favorite.destroy
-    # @q = Post.ransack(params[:q])
-    # @user = User.find(params[:user_id])
-    # @favorite_posts = @q.result(distinct: true).joins(:favorites).where(favorites: {user_id: @user.id}).page(params[:favorite_posts_page]).per(6).order(created_at: "DESC")
   end
 end
