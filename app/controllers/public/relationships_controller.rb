e2c8a52bd79e45b377ee561698429a82f6a4e82d
nitamago205 <3ejiz428@gmail.com>
2022-06-23 03:52:45 +0000

    フォロー/フォローワー一覧画面修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,12 +14,12 @@ class Public::RelationshipsController < ApplicationController
   end
   # フォロー一覧
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page]).per(10)
   end
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page]).per(10)
   end
 end
